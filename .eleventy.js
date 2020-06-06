a0d19082f2fb392a84d2c3c8f1002c29073d095f
LeiHeiI <65061933+LeiHeiI@users.noreply.github.com>
2020-06-06 15:14:28 +0800

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("toISOString", function(date) {
     return date.toISOString().split('T')[0];
-    
+  });
     // Folders to copy into output.
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("css");
 
     // If we use Netlify and has the _redirects file.
     eleventyConfig.addPassthroughCopy("_redirects");
-
-  });
 }
