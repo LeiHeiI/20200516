5a9bbb09d481ce57a8355e2ba06bc866add9ef3a
LeiHeiI <65061933+LeiHeiI@users.noreply.github.com>
2020-06-27 15:26:08 +0800

    Create login.js

---
+++ _functions/login.js
@@ -0,0 +1,22 @@
+exports.handler = (event, context, callback) => {
+  // accesscode
+  console.log("---Event---");
+  console.log(event);  
+
+  console.log("---Context---");
+  console.log(context);
+
+  console.log("---Form Data---");
+  console.log(event.body)
+
+  result = "/"
+
+  if (event.body == "accesscode=demo") {
+    result = "/super-long-url-here-to-hide-the-gated-content/"
+  }
+
+  callback(null, {
+    statusCode: 200,
+    body: result
+  });
+}
